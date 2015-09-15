faa212acfcad25a10a7adfb871eafbf7868a3bc5
Shlomi <shlomisha@users.noreply.github.com>
2015-09-15 16:24:37 +0300

    Create Favez0ne_Ads_Links_Remover.meta.js

---
+++ Favez0ne_Ads_Links_Remover.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @id             Favez0ne_Ads_Links_Remover
+// @name           Favez0ne Ads Links Remover
+// @version        1.1.0
+// @namespace      shlomisha
+// @author         shlomisha
+// @description    Avoids the ads page and redirects to the downloading link immediately. In addition, opens the links set
+// @include        http://www.favez0ne.net/search.php
+// @grant          none
+// @run-at         document-end
+// @updateURL      https://raw.githubusercontent.com/shlomisha/Favez0ne-Ads-Links-Remover/master/Favez0ne_Ads_Links_Remover.meta.js
+// @downloadURL    https://raw.githubusercontent.com/shlomisha/Favez0ne-Ads-Links-Remover/master/Favez0ne_Ads_Links_Remover.user.js
+// ==/UserScript==
